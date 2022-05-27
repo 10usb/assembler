2fa795258d751e2085b5d52c2e0e27e4ebdaa2e1
10usb <t.bruins@gmail.com>
2022-05-27 08:31:46 +0200

    Added concept class to store marco data

---
+++ Assembler/Macro.cs
@@ -0,0 +1,55 @@
+﻿using Assembler.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembler {
+    /// <summary>
+    /// A template instruction containing a template of instructions
+    /// </summary>
+    public class Macro {
+        private readonly Macro parent;
+        private readonly string name;
+        private readonly string[] arguments;
+        private readonly List<AssemblyLine> lines;
+
+        /// <summary>
+        /// Constructs an empty macro
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="name"></param>
+        /// <param name="arguments"></param>
+        public Macro(Macro parent, string name, string[] arguments) {
+            this.parent = parent;
+            this.name = name;
+            this.arguments = arguments;
+            lines = new List<AssemblyLine>();
+        }
+
+        /// <summary>
+        /// Add a line of assembly to this macro
+        /// </summary>
+        /// <param name="line"></param>
+        public void Add(AssemblyLine line) {
+            lines.Add(line);
+        }
+
+        /// <summary>
+        /// Finds a matching macro
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="argumentCount"></param>
+        /// <returns></returns>
+        public Macro Find(string name, int argumentCount) {
+            if (this.name == name && arguments.Length == argumentCount)
+                return this;
+
+            if (parent != null)
+                return parent.Find(name, argumentCount);
+
+            return null;
+        }
+    }
+}
---
+++ Assembler/Processors/MacroProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembler.Processors {
+    public class MacroProcessor : IProcessor {
+        public void ProcessLine(AssemblyLine line) {
+            throw new NotImplementedException();
+        }
+    }
+}

778675b98c9a97153469a9e24ead266f3315acb8
10usb <t.bruins@gmail.com>
2022-05-30 22:56:20 +0200

    Added commandline argument parsing

--- Assembler/Program.cs
+++ Assembler/Program.cs
@@ -1,35 +1,89 @@
 ﻿using Assembler.Processors;
-using Assembler.Values;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Assembler {
     class Program {
+        private static FileInfo source;
+        private static FileInfo output;
+        private static bool wait;
 
-        static void Main(string[] args) {
-            FileInfo source = new FileInfo("I:\\Programming\\Assembler\\Resources\\source.asm");
-            FileInfo output = new FileInfo("I:\\Programming\\Assembler\\Resources\\source.bin");
-
+        static int Main(string[] args) {
             try {
-                using (Document document = new Document(output)) {
-                    Parser parser = new Parser(new Processor(document));
+                if (!Parse(args))
+                    return 0;
 
-                    using (StreamReader reader = source.OpenText()) {
-                        parser.Parse(reader);
-                    }
-                }
+                Assemble();
+
+                if (wait) Console.ReadKey();
+                return 0;
             } catch (AssemblerException e) {
                 Console.Error.WriteLine("{0} on line {1}", e.Message, e.LineNr);
+            } catch (Exception ex) {
+                Console.Error.WriteLine(ex.Message);
             }
 
-            Console.ReadKey();
+            if (wait) Console.ReadKey();
+            return 1;
         }
 
+        private static void ShowHelp() {
+            Console.WriteLine("Usage: assembler [options...] filename");
+            Console.WriteLine();
+            Console.WriteLine("Options");
+            Console.WriteLine(" -o <file>         write output to file");
+            Console.WriteLine(" --wait            wait for any key");
+        }
+
+        private static bool Parse(string[] args) {
+            IEnumerator<string> enumerator = args.ToList().GetEnumerator();
+            if (!enumerator.MoveNext()) {
+                ShowHelp();
+                return false;
+            }
+
+            while (enumerator.Current[0] == '-') {
+                switch (enumerator.Current) {
+                    case "-o": {
+                        if (!enumerator.MoveNext())
+                            throw new Exception("Missing argument for -0");
+                        output = new FileInfo(enumerator.Current);
+                    }
+                    break;
+                    case "--wait": wait = true; break;
+                    default: throw new Exception(string.Format("Unknown commandline option '{0}'", enumerator.Current));
+                }
+
+                if (!enumerator.MoveNext())
+                    throw new Exception("No source file specified");
+            }
+
+            source = new FileInfo(enumerator.Current);
+
+            if (enumerator.MoveNext())
+                throw new Exception(string.Format("Unexpected output {0}", enumerator.Current));
+
+            return true;
+        }
+
+        private static void GenerateOutput() {
+            string filename = source.Name.Substring(0, source.Name.Length - source.Extension.Length) + ".o";
+            output = new FileInfo(Path.Combine(source.DirectoryName, filename));
+        }
+
+        private static void Assemble() {
+            if (output == null)
+                GenerateOutput();
+
+            using (Document document = new Document(output)) {
+                Parser parser = new Parser(new Processor(document));
+
+                using (StreamReader reader = source.OpenText()) {
+                    parser.Parse(reader);
+                }
+            }
+        }
     }
 }

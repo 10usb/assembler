a460210b886c1c5ee056327ca2d5355243ae495f
10usb <t.bruins@gmail.com>
2022-06-03 23:05:24 +0200

    Added some comments

--- Assembler/AssemblyLine.cs
+++ Assembler/AssemblyLine.cs
@@ -2,38 +2,73 @@
 using System.Text;
 
 namespace Assembler {
+    /// <summary>
+    /// An assembly line is a carrier for all the information that can be stored
+    /// into a single line of assembly. It will usually be provided by the parser
+    /// to the interpreters.
+    /// </summary>
     public class AssemblyLine {
         private readonly int lineNr;
         private string label;
-        private string scope;
         private string assignment;
         private string modifier;
         private string instruction;
         private string comments;
         private IValue[] arguments = new IValue[0];
 
+        /// <summary>
+        /// TODO add source file reference
+        /// </summary>
+        /// <param name="lineNr"></param>
         public AssemblyLine(int lineNr) {
             this.lineNr = lineNr;
         }
 
+        /// <summary>
+        /// The line number in the source file this assembly line was found
+        /// </summary>
         public int LineNumber { get => lineNr; }
 
+        /// <summary>
+        /// The label name is defined
+        /// </summary>
         public string Label {
             get { return label; }
             set { label = string.IsNullOrWhiteSpace(value) ? null : value; }
         }
+
+        /// <summary>
+        /// The target scope when assigning a variable
+        /// </summary>
+        public ScopeType Scope { get; set; } = ScopeType.None;
+
+        /// <summary>
+        /// The name of the varriable to assign
+        /// </summary>
         public string Assignment {
             get { return assignment; }
             set { assignment = string.IsNullOrWhiteSpace(value) ? null : value; }
         }
+
+        /// <summary>
+        /// Modifier prefixing an instruction
+        /// </summary>
         public string Modifier {
             get { return modifier; }
             set { modifier = string.IsNullOrWhiteSpace(value) ? null : value; }
         }
+
+        /// <summary>
+        /// The name of the instruction
+        /// </summary>
         public string Instruction {
             get { return instruction; }
             set { instruction = string.IsNullOrWhiteSpace(value) ? null : value; }
         }
+
+        /// <summary>
+        /// The argument for an assignment of arguments for a instrction
+        /// </summary>
         public IValue[] Arguments {
             get => arguments;
             set {
@@ -44,17 +79,34 @@ namespace Assembler {
                 }
             }
         }
+
+        /// <summary>
+        /// Does this instrction have a template block after it
+        /// </summary>
         public bool IsBlockOpen { get; set; }
+
+        /// <summary>
+        /// Is this the end of a template block
+        /// </summary>
         public bool IsBlockClose { get; set; }
+
+        /// <summary>
+        /// Comment on the line including the comment start token
+        /// </summary>
         public string Comments {
             get { return comments; }
             set { comments = string.IsNullOrWhiteSpace(value) ? null : value; }
         }
 
-        public ScopeType Scope { get; set; } = ScopeType.None;
-
+        /// <summary>
+        /// The start of a conditional section
+        /// </summary>
         public ConditionalSection Section { get; set; }
 
+        /// <summary>
+        /// An assembly-ish code representation of an assembly line
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() {
             StringBuilder builder = new StringBuilder();
             if (Label != null)
--- Assembler/Writer.cs
+++ Assembler/Writer.cs
@@ -6,11 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Assembler {
+    /// <summary>
+    /// The writer is responsible for actually writing the bytes to an output file.
+    /// And while doing so it keeps track of the current bytes written and the virtual
+    /// offset.
+    /// </summary>
     public class Writer : IDisposable {
         private readonly Stream stream;
         private long origin;
         private long offset;
 
+        /// <summary>
+        /// The current origin the offset is pointing to
+        /// </summary>
         public long Origin {
             get { return origin; }
             set {
@@ -19,54 +27,94 @@ namespace Assembler {
             }
         }
 
+        /// <summary>
+        /// The current offset to the origin the next bytes will be written
+        /// </summary>
         public long Offset {
             get {
                 return offset;
             }
         }
 
+        /// <summary>
+        /// The current virtual address
+        /// </summary>
         public long Position {
             get {
                 return origin + offset;
             }
         }
 
+        /// <summary>
+        /// The current number of written bytes
+        /// </summary>
         public long FileOffset {
             get {
                 return stream.Position;
             }
         }
 
+        /// <summary>
+        /// Constructs a writer pointing to an output stream
+        /// </summary>
+        /// <param name="stream"></param>
         public Writer(Stream stream) {
             this.stream = stream;
         }
 
+        /// <summary>
+        /// Seet to a fixed offset into the output file for setting a byte value
+        /// </summary>
+        /// <param name="offset"></param>
         internal void Seek(long offset) {
             stream.Position = offset;
         }
 
+        /// <summary>
+        /// Set a byte value at the current location in the file
+        /// </summary>
+        /// <param name="value"></param>
         internal void SetByte(long value) {
             byte[] data = new byte[] { (byte)value };
             stream.Write(data, 0, data.Length);
         }
 
+        /// <summary>
+        /// Writes a string value as UTF-8 and incrementing the offset
+        /// </summary>
+        /// <param name="text"></param>
         internal void WriteString(string text) {
             Write(Encoding.UTF8.GetBytes(text));
         }
 
+        /// <summary>
+        /// Write a single byte value and incrementing the offset
+        /// </summary>
+        /// <param name="value"></param>
         public void WriteByte(long value) {
             Write(new byte[] { (byte)value });
         }
 
+        /// <summary>
+        /// Writes an byte array and incrementing the offset
+        /// </summary>
+        /// <param name="data"></param>
         private void Write(byte[] data) {
             offset += data.Length;
             stream.Write(data, 0, data.Length);
         }
 
+        /// <summary>
+        /// Flushes any buffered data to the file
+        /// </summary>
         public void Flush() {
             stream.Flush();
         }
 
+        /// <summary>
+        /// Makes sure the last changes to the file are flushed
+        /// and then the stream is closed
+        /// </summary>
         public void Dispose() {
             stream.Flush();
             stream.Close();

88e9cb1eaf4fae46c3265ae5196954076cfd2be3
10usb <t.bruins@gmail.com>
2022-06-15 21:44:26 +0200

    Added support in the AssemblerException to accept a trace instead of a line number

--- Assembler/AssemblerException.cs
+++ Assembler/AssemblerException.cs
@@ -8,9 +8,17 @@ namespace Assembler {
     public class AssemblerException : Exception {
         public int LineNr { get; }
 
+        public Trace Trace { get; }
+
         public AssemblerException(string message, int lineNr, params object[] arguments)
             : base(string.Format(message, arguments)) {
             LineNr = lineNr;
+        }
+
+        public AssemblerException(Trace trace, string message, params object[] arguments)
+            : base(string.Format(message, arguments)) {
+            Trace = trace;
+            LineNr = trace.LineNumber;
         }
     }
 }

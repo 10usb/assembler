05c7e57975631474001287d275e12401b39c253a
10usb <t.bruins@gmail.com>
2022-06-18 23:36:54 +0200

    Added support for negative numbers and underscores in variable names

--- Assembler/Parser.cs
+++ Assembler/Parser.cs
@@ -10,8 +10,8 @@ using System.Threading.Tasks;
 
 namespace Assembler {
     public class Parser : IDisposable {
-        private static readonly Regex linePattern = new Regex(@"^(?:\s*([a-zA-Z0-9_]+):)?\s*(?:(?:(?:(?:(global|local|const)\s+)?([a-zA-Z0-9]+)\s*=|(?:([+\-&!*?$%=~}]+)\s*)?([a-zA-Z0-9]+))\s*(.*?)\s*({)?)|(}))?\s*(;.+)?$", RegexOptions.Compiled);
-        private static readonly Regex valueRegex = new Regex(@"^\s*(?:([1-9][0-9]*\b|0\b)|(0x[0-9a-fA-F]+\b)|(0[0-7]+\b)|([01]+b\b)|([a-zA-Z$_][a-zA-Z0-9$_]*)|""([^""]*(?:""""[^""]*)*)""|(((?<open>\()[^()]*)+([^()]*(?<-open>\)))+(?(open)(?!))))", RegexOptions.Compiled);
+        private static readonly Regex linePattern = new Regex(@"^(?:\s*([a-zA-Z0-9_]+):)?\s*(?:(?:(?:(?:(global|local|const)\s+)?([a-zA-Z0-9_]+)\s*=|(?:([+\-&!*?$%=~}]+)\s*)?([a-zA-Z0-9]+))\s*(.*?)\s*({)?)|(}))?\s*(;.+)?$", RegexOptions.Compiled);
+        private static readonly Regex valueRegex = new Regex(@"^\s*(?:(-?[1-9][0-9]*\b|0\b)|(0x[0-9a-fA-F]+\b)|(0[0-7]+\b)|([01]+b\b)|([a-zA-Z$_][a-zA-Z0-9$_]*)|""([^""]*(?:""""[^""]*)*)""|(((?<open>\()[^()]*)+([^()]*(?<-open>\)))+(?(open)(?!))))", RegexOptions.Compiled);
         private static readonly Regex operatorRegex = new Regex(@"^\s*(<<|>>|>=|<=|!=|is not|is|as|[+\-*/%|=\^<>&])\s*", RegexOptions.Compiled);
 
         private const int GROUP_DECIMAL = 1;
